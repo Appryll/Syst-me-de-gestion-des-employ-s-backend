8065ae3fe9e836f763460e1e805c7b4efcc9862d
Natalia FABIANO <natalia.fabiano@hotmail.com>
2023-07-26 11:48:26 +0200

    update + delete employee

--- src/main/java/com/employes/backend/controller/EmployeeController.java
+++ src/main/java/com/employes/backend/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.employes.backend.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -48,5 +50,16 @@ public class EmployeeController {
 		
 		Employee updateEmployee = employeeRepository.save(employee);
 		return ResponseEntity.ok(updateEmployee);
+	}
+
+	// delete employee rest api
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+		Employee employee = employeeRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id : " + id));
+
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("delete", Boolean.TRUE);
+		return ResponseEntity.ok(response);
 	}
 }
